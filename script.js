96830fae752ca68ae502e7551529853267a30f84
ViniciusVieira-web <viniciusvieiramorim76@gmail.com>
2024-10-14 21:06:33 -0300

    implementação de js

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+    let popup = document.getElementById('popup')
+    let maincontent = document.getElementById('main-content')
+
+    popup.style.visibility = "visible";
+
+    setTimeout(function() {
+        popup.classList.add("hidden");
+        setTimeout(function(){
+            maincontent.style.display = "block";
+            // document.body.style.overflow = "auto";
+        }, 1000);  
+    }, 3000);
+};
