4f47bf163aec17273d672a4df691264ae788b628
ViniciusVieira-web <viniciusvieiramorim76@gmail.com>
2024-10-17 10:23:49 -0300

    atualização

--- script.js
+++ script.js
@@ -8,25 +8,8 @@ window.onload = function(){
         popup.classList.add("hidden");
         setTimeout(function(){
             maincontent.style.display = "block";
-            // document.body.style.overflow = "auto";
+            document.body.style.overflow = "auto";
         }, 500);  
     }, 1500);
+
 };
-
-// const swiper = new swiper('.swiper',{
-//     direction: 'vertical',
-//     loop: true, 
-
-//     pagination:{
-//         el: '.swiper-pagination',
-//     },
-
-//     navigation:{
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-
-//     scrollbar:{
-//         el: '.swiper-scrollbar',
-//     },
-// });
